a95dbcef54a168e2f1a20010380201f233b1fad2
islakhun24 <islakhunnurdzaki24@gmail.com>
2022-02-15 00:22:30 +0700

    db

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "db_galon",
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "b7d14c76a9d8fd",
+    PASSWORD: "1cb1fa91",
+    DB: "heroku_97278bef1eda041",
     dialect: "mysql",
     pool: {
       max: 5,
