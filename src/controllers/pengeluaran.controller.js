370ed97062e42f68ade1b081cdb4495dcd469bcc
islakhun24 <islakhunnurdzaki24@gmail.com>
2022-02-19 00:37:46 +0700

    kjkj

--- src/controllers/pengeluaran.controller.js
+++ src/controllers/pengeluaran.controller.js
@@ -1,6 +1,6 @@
 const db =require('../models')
 const Pengeluaran = db.pengeluaran;
-
+var moment = require('moment');
 exports.create_pengeluaran = (req, res)=>{
     const {
         nama_pengeluaran, satuan, jumlah, harga, keterangan, date,
@@ -46,7 +46,17 @@ exports.list_pengeluaran = (req, res)=>{
     Pengeluaran.findAll().then((result) => {
         return res.status(200).send({
             message: 'Berhasil menampilkan list Pengeluaran',
-            data: result
+            data: result.map(val=>{
+                return {
+                    id: val.id,
+                    nama_pengeluaran: val.nama_pengeluaran,
+                    satuan: val.satuan,
+                    jumlah: val.jumlah,
+                    harga: val.harga,
+                    keterangan: val.keterangan,
+                    date: moment(val.date).format('DD-MM-YYYY'),
+                }
+            })
         });
     }).catch((err) => {
         console.log(err);
